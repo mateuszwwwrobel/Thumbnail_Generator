76e6c6b30140730a9d3f52964db2ad9ad6325de0
mateuszwwwrobel <mateusz.wwwrobel@gmail.com>
2021-05-06 21:49:26 +0100

    Add API endpoints.

--- main.py
+++ main.py
@@ -1,35 +1,57 @@
-from fastapi import FastAPI, Response, status
+from fastapi import FastAPI, File, UploadFile, Response, status
+from fastapi.responses import FileResponse
+import uuid
+from utils import validate_mime_type, upload_file_to_s3, get_file_from_s3, resize_image
+from fastapi.responses import HTMLResponse
+import io
 
-from PIL import Image
 
+IMAGEDIR = "/home/matt/Desktop/Dlabs/Thumbnail_Generator/test/fastapi-images/"
 
 app = FastAPI()
 
 
-images = []
-
-
 @app.get("/")
-def root():
-    return "Welcome in Thumbnail Generator"
+async def main():
+    content = """
+<body>
+<form action="/images" enctype="multipart/form-data" method="post">
+<input name="files" type="file" multiple>
+<input type="submit">
+</form>
+</body>
+    """
+    return HTMLResponse(content=content)
 
 
-@app.post('/images')
-def save_image():
-    image = Image.open('earth.jpg')
+@app.post("/images/")
+async def create_upload_file(response: Response, file: UploadFile = File(...)):
+    # opcja na plik z linku też?
 
-    images.append(image)
-    print(images)
+    if validate_mime_type(file.content_type):
+        file.filename = f"{uuid.uuid4()}.{file.content_type.split('/')[1]}"
+        contents = await file.read()
+        upload_file_to_s3(io.BytesIO(contents), 'thumb-images-bucket', file.filename)
+        return {"filename": file.filename}
 
-    return {"message": "Photo added to database."}
+    response.status_code = status.HTTP_400_BAD_REQUEST
+    return {"message": f"Invalid file format."}
 
 
 @app.get('/images/{dimensions}')
-def get_thumbnail(dimensions: str, response: Response):
+async def get_thumbnail(dimensions: str, response: Response):
     try:
         width, height = [int(dimension) for dimension in dimensions.split('x')]
     except ValueError:
         response.status_code = status.HTTP_400_BAD_REQUEST
         return {"message": f"Your URL '/images/{dimensions}' is invalid. Please try again."}
 
-    return {"width": width, "height": height}
+    img = get_file_from_s3('dasd', 'thumb-images-bucket')
+    if img:
+        resized_image_url = resize_image(img, width, height)
+
+        return resized_image_url
+        # return FileResponse(resized_image_url)
+
+    response.status_code = status.HTTP_404_NOT_FOUND
+    return {"message": f"Please upload images in order to create a thumbnail."}
